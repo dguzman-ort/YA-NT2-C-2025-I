552b3db8df182b006efba3b22096f9ce5249bbfb
Daniel Guzman <dguzman1012@gmail.com>
2025-05-06 23:03:47 -0300

    Ejemplo contador

--- React/hooks/src/App.jsx
+++ React/hooks/src/App.jsx
@@ -2,41 +2,48 @@ import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Cronometro from './components/Cronometro'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [nombre, setNombre] = useState("Desconocido")
-  const [edad, setEdad] = useState(20)
+  
+  const [estaIniciado, setEstaIniciado] = useState(false)
 
 
-  useEffect(() => {
-    console.log("1.- Inicializacion del componente")
-    return () => {
-      console.log("3.- Estamos en el ciclo de vida UNMOUNT: apenas se remueve este componente me ejecuto!")
-    }
+
+  // const [nombre, setNombre] = useState("Desconocido")
+  // const [edad, setEdad] = useState(20)
 
 
-  }, [])
+  // useEffect(() => {
+  //   console.log("1.- Inicializacion del componente")
+  //   return () => {
+  //     console.log("3.- Estamos en el ciclo de vida UNMOUNT: apenas se remueve este componente me ejecuto!")
+  //   }
 
-  useEffect(() => {
-    console.log("2.- Cambio el estado nombre: ", nombre)
-  }, [nombre])
 
-  useEffect(() => {
-    console.log("2.- Cambio el estado edad: ", edad)
-  }, [edad])
+  // }, [])
 
-  useEffect(() => {
-    console.log("2.- Cambio uno de los dos estados: ", nombre, edad)
-  }, [nombre, edad])
+  // useEffect(() => {
+  //   console.log("2.- Cambio el estado nombre: ", nombre)
+  // }, [nombre])
+
+  // useEffect(() => {
+  //   console.log("2.- Cambio el estado edad: ", edad)
+  // }, [edad])
+
+  // useEffect(() => {
+  //   console.log("2.- Cambio uno de los dos estados: ", nombre, edad)
+  // }, [nombre, edad])
 
 
 
   return (
     <>
-      <div><h1>00:00</h1></div>
+      <Cronometro estaIniciado={estaIniciado} />
       <div className="card">
-        <button>Iniciar | Pausar</button>
+        <button onClick={() => setEstaIniciado(!estaIniciado)}>{
+          estaIniciado? 'Pausar': 'Iniciar'
+        }</button>
         <button>Reiniciar</button>
       </div>        
       
---
+++ React/hooks/src/components/Cronometro/index.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react"
+
+let intervalID
+
+export default ({estaIniciado}) => {
+  const [count, setCount] = useState(0)
+
+
+  useEffect(() => {
+    console.log("Detecto el cambio del prop", estaIniciado)
+    if (estaIniciado){
+      intervalID = setInterval(() => {   
+        //console.log(`Valor del contador: ${count}`)     
+        // setCount(count + 1)
+        
+        setCount((valorPrevio) => {
+          console.log(`Valor del contador: ${valorPrevio}`)
+          return valorPrevio + 1
+
+        })
+
+      },1000)
+
+      console.log("ID interval: ", intervalID)
+    }else{
+      // Limpio el interval
+      clearInterval(intervalID)
+    }
+  
+    // 
+  }, [estaIniciado])
+
+  return (
+    <div><h1>{count}</h1></div>
+  )
+}
